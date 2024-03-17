d584a28528c0eabfa3f098ba2b485a94f769fd37
Игорь <flagmr040@gmail.com>
2024-03-17 19:00:53 +0200

    Bugs fixed

--- src/Homework15.java
+++ src/Homework15.java
@@ -1,13 +1,13 @@
 public class Homework15 {
     public static void main(String[] args) {
-        // Создаем объекты класса Person и вызываем метод personInfo() для каждого объекта
-        Person person1 = new Person("Will", "Smith", "New York", "2936729462846");
-        System.out.println(person1.personInfo());
+        Person person = new Person();
 
-        Person person2 = new Person("Jackie", "Chan", "Shanghai", "12312412412");
-        System.out.println(person2.personInfo());
+        String info1 = person.personInfo("Will", "Smith", "New York", "2936729462846");
+        String info2 = person.personInfo("Jackie", "Chan", "Shanghai", "12312412412");
+        String info3 = person.personInfo("Sherlock", "Holmes", "London", "37742123513");
 
-        Person person3 = new Person("Sherlock", "Holmes", "London", "37742123513");
-        System.out.println(person3.personInfo());
+        System.out.println(info1);
+        System.out.println(info2);
+        System.out.println(info3);
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,17 +1,5 @@
 public class Person {
-    private String firstName;
-    private String lastName;
-    private String city;
-    private String phoneNumber;
-
-    public Person(String firstName, String lastName, String city, String phoneNumber) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.city = city;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String personInfo() {
+    public String personInfo(String firstName, String lastName, String city, String phoneNumber) {
         return "Зателефонувати громадянинові " + firstName + " " + lastName + " з міста " + city + " можна за номером " + phoneNumber + ".";
     }
 }
